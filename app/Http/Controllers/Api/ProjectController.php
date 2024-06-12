501ce1d8dbdb97df7acb96a002c82c89bb8e27c7
Marcomagnano93 <marcog.magnano@gmail.com>
2024-06-12 14:49:15 +0200

    ADD: milstone 2 [api.php route]

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,9 +9,9 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(Request $request){
-
+        
         $projects = Dashboard::all();
-        $projects = Dashboard::with('types', 'technologies', 'user')->get();
+
 
         return response()->json([
             
